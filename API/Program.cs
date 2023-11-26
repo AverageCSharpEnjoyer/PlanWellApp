a6aefe0136ae2a41052ac8afd6f1bb5e11307da8
Martyna <zielinska0@outlook.com>
2023-11-26 14:48:28 +0100

    AddingViteAndAxios

--- API/Program.cs
+++ API/Program.cs
@@ -10,6 +10,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>();
+builder.Services.AddCors(options => 
+{
+    options.AddPolicy("CorsPolicy", policy => 
+    {
+        policy.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -19,6 +26,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react';
 import './App.css'
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/meal')
+      .then(response => {
+        setMeals(response.data)
+      })
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>MealApp</h1>
+      <ul>
+        {meals.map((meal: any) => (
+          <li key={meal.id}>
+            {meal.name}
+          </li>  
+        ))}
+      </ul>
     </>
   )
 }
